87011e1008f596dfa7b895b5cb96fd68c1eed39c
Andriy <Andriy@DESKTOP-9LBFKE8>
2019-04-26 22:00:50 +0300

    ппп

--- Service/Class1.cs
+++ Service/Class1.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IServise
+namespace Service
 {
     public class Class1
     {
-
     }
 }
--- Service/Properties/AssemblyInfo.cs
+++ Service/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IServise")]
+[assembly: AssemblyTitle("Service")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IServise")]
+[assembly: AssemblyProduct("Service")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b549baad-2062-4006-9177-85d0b32efcff")]
+[assembly: Guid("fd2a15da-15e3-4efc-8769-126f9fc02a16")]
 
 // Version information for an assembly consists of the following four values:
 //
