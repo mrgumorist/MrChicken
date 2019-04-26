3dfb7e2b3733b4840e332a29ddd0ecee5daa9af2
Andriy <Andriy@DESKTOP-9LBFKE8>
2019-04-26 23:19:05 +0300

    AddTableUsers

---
+++ IService/Entities/Users.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IService
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        public string TelegramID { get; set; }
+        public string Email { get; set; }
+        public bool ISConfirmed { get; set; }
+        [Required]
+        public DateTime DateOfRegister { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public bool DoesWantRecomendations { get; set; }
+    }
+}
